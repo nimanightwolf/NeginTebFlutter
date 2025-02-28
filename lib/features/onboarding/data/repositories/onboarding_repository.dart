da8762af5abc6fa5a153fdacc8c5d80b82169f9e
Amir Hossein Mohammadi <amir00462@gmail.com>
2025-02-28 11:56:19 +0300

    add onboarding repository

---
+++ lib/features/onboarding/data/repositories/onboarding_repository.dart
@@ -0,0 +1,19 @@
+class OnboardingRepository {
+  final List<Map<String, String>> onboardingData = [
+    {
+      "title": "بهترین هتل های ایران و جهان",
+      "description": "تجربه اقامت در بهترین و اقتصادی ترین هتل ها در سراسر جهان را با تیم ما تجربه کنید.",
+      "image": "assets/images/onboarding_1.png"
+    },
+    {
+      "title": "تنوع محل های اقامتی",
+      "description": "بزرگ‌ترین اپلیکیشن اقامتی با بیش از 200 نوع هتل، متل، اتاق و سوییت...",
+      "image": "assets/images/onboarding_2.png"
+    },
+    {
+      "title": "مناسب‌ترین قیمت ها",
+      "description": "با توجه به بودجه شما، ما مناسب‌ترین محل اقامتی را به شما پیشنهاد می‌دهیم.",
+      "image": "assets/images/onboarding_3.png"
+    },
+  ];
+}
