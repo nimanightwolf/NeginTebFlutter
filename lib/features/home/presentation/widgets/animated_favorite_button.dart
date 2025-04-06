a20cb84e5495ed672f7221f258d0cd6791fabbfc
Amir Hossein Mohammadi <amir00462@gmail.com>
2025-04-06 08:43:44 +0300

    add glassmorphism widget to shared widgets

---
+++ lib/features/home/presentation/widgets/animated_favorite_button.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class AnimatedFavoriteButton extends StatefulWidget {
+  final bool isFavorite;
+  final VoidCallback onTap;
+
+  const AnimatedFavoriteButton({
+    super.key,
+    required this.isFavorite,
+    required this.onTap,
+  });
+
+  @override
+  AnimatedFavoriteButtonState createState() => AnimatedFavoriteButtonState();
+}
+
+class AnimatedFavoriteButtonState extends State<AnimatedFavoriteButton> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 250),
+    );
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.25).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.elasticOut),
+    );
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTap() {
+    widget.onTap();
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: GlassMorphism(
+        borderRadius: 50,
+        blur: 3,
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.white60, width: 0.5),
+          ),
+          child: AnimatedBuilder(
+            animation: _scaleAnimation,
+            builder: (context, child) {
+              return Transform.scale(
+                scale: _scaleAnimation.value,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white60.withValues(alpha: 0.1),
+                  child: Icon(
+                    Icons.favorite_rounded,
+                    color: widget.isFavorite ? Colors.red : Colors.white,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
