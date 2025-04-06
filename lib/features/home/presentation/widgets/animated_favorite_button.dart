1be9759d87ecbe886670a07638e0a9d097a685e7
Amir Hossein Mohammadi <amir00462@gmail.com>
2025-04-06 08:58:14 +0300

    add AnimatedFavoriteButton

--- lib/features/home/presentation/widgets/animated_favorite_button.dart
+++ lib/features/home/presentation/widgets/animated_favorite_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hotelino/shared/widgets/glassmorphism.dart';
 
 class AnimatedFavoriteButton extends StatefulWidget {
   final bool isFavorite;
