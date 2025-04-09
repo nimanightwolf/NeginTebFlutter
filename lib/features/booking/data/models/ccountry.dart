8b55400a8b36328fd2a80e5b2b57248186e872a6
Amir Hossein Mohammadi <amir00462@gmail.com>
2025-04-09 15:24:34 +0300

    add Country data class

---
+++ lib/features/booking/data/models/ccountry.dart
@@ -0,0 +1,7 @@
+class Country {
+  final String name;
+  final String dialCode;
+  final String countryCode;
+
+  Country({required this.name, required this.dialCode, required this.countryCode});
+}
