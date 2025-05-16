12ec263eaf13e4d60c86e143c7ab32e7ea66fd48
Amir Hossein Mohammadi <amir00462@gmail.com>
2025-05-16 09:14:17 +0300

    add profile picture in profile_page

--- lib/features/profile/presentation/profile_page.dart
+++ lib/features/profile/presentation/profile_page.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'dart:io';
+import 'package:hotelino/features/home/presentation/provider/profile_provider.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -26,6 +30,54 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<ProfileProvider>(
+      builder: (context, profileProvider, child) {
+        final profile = profileProvider.profile;
+
+        if (profile == null) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return Scaffold(
+          body: SafeArea(
+              child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 24,
+                ),
+                Stack(
+                  children: [
+                    CircleAvatar(
+                        radius: 55,
+                        backgroundImage: _image != null
+                            ? FileImage(_image!)
+                            : NetworkImage(profile.avatarUrl) as ImageProvider),
+                    Positioned(
+                        right: 0,
+                        bottom: 0,
+                        child: GestureDetector(
+                          onTap: _pickImage,
+                          child: Container(
+                            padding: EdgeInsets.all(6),
+                            decoration: BoxDecoration(
+                                color: Theme.of(context).colorScheme.primaryFixed, shape: BoxShape.circle),
+                            child: Icon(
+                              Icons.edit,
+                              size: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )),
+                  ],
+                ),
+              ],
+            ),
+          )),
+        );
+      },
+    );
   }
 }
