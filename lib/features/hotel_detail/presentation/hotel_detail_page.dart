0c05e09f5e0430dc2a26c3cff87b139c081bfa2c
Amir Hossein Mohammadi <amir00462@gmail.com>
2025-05-24 22:23:42 +0300

    add address and name of hotel in hotel_detail_page

--- lib/features/hotel_detail/presentation/hotel_detail_page.dart
+++ lib/features/hotel_detail/presentation/hotel_detail_page.dart
@@ -3,6 +3,7 @@ import 'package:hotelino/core/utils/network.dart';
 import 'package:hotelino/features/home/data/models/hotel.dart';
 import 'package:hotelino/features/home/data/repositories/hotel_repository.dart';
 import 'package:hotelino/shared/services/json_data_service.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
 class HotelDetailPage extends StatelessWidget {
   const HotelDetailPage({super.key, required this.hotelId});
@@ -36,6 +37,54 @@ class HotelDetailPage extends StatelessWidget {
                   background: GestureDetector(
                     onLongPress: () {},
                     child: Image.network(fit: BoxFit.cover, networkUrl(hotel.images.first)),
+                  ),
+                ),
+                elevation: 8,
+                expandedHeight: 300,
+                leading: IconButton(
+                    onPressed: () {
+                      PersistentNavBarNavigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    )),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        hotel.name,
+                        style: textTheme.headlineMedium,
+                        textDirection: TextDirection.rtl,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              hotel.address,
+                              textAlign: TextAlign.right,
+                              textDirection: TextDirection.rtl,
+                              style: TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 6,
+                          ),
+                          Icon(
+                            Icons.location_on_outlined,
+                            size: 18,
+                            color: Colors.grey,
+                          )
+                        ],
+                      )
+                    ],
                   ),
                 ),
               ),
