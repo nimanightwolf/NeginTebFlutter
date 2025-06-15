d396ad41014b20cc5fc85b0d9a0a1a4dd5e3ef8e
Amir Hossein Mohammadi <amir00462@gmail.com>
2025-06-15 08:54:25 +0300

    full screen map page desing finished

--- lib/features/hotel_detail/presentation/full_screen_map_page.dart
+++ lib/features/hotel_detail/presentation/full_screen_map_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class FullScreenMapPage extends StatelessWidget {
   final double latitude;
@@ -10,6 +12,52 @@ class FullScreenMapPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      body: FlutterMap(
+          options: MapOptions(
+              initialCenter: LatLng(latitude, longitude),
+              initialZoom: 15.0,
+              interactionOptions: InteractionOptions(flags: InteractiveFlag.all)),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'ir.dunijet.hotelino',
+            ),
+            MarkerLayer(
+              markers: [
+                Marker(
+                  point: LatLng(latitude, longitude),
+                  width: 80,
+                  height: 80,
+                  child: Column(
+                    children: [
+                      const Icon(
+                        Icons.location_pin,
+                        color: Colors.red,
+                        size: 40,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                        color: Colors.white.withOpacity(0.8),
+                        child: Text(
+                          hotelName,
+                          style: textTheme.bodySmall!.copyWith(
+                            color: Colors.black,
+                          ),
+                          textDirection: TextDirection.rtl,
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ]),
+    );
   }
 }
